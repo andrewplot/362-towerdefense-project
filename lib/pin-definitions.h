0753cc0dbcae778f4922ae9a38f84759d6fe7ae6
smbeane <smbeane5235@gmail.com>
2025-11-04 13:25:18 -0500

    start schematic and matrix

---
+++ lib/pin-definitions.h
@@ -0,0 +1,21 @@
+#ifndef PIN_DEFINITIONS_H
+#define PIN_DEFINITIONS_H
+
+
+// LED Matrix
+#define R1 5
+#define B1 6
+#define G1 7
+#define R2 9
+#define B2 10
+#define G2 11
+#define E 12
+#define A 13
+#define B 14
+#define C 15
+#define D 16
+#define CLK 17
+#define LAT 18
+#define OE 19
+
+#endif

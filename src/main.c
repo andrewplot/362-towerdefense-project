310d86c34dc66645671efff0c2a5e78a84edf8de
Rudra Raj <raj48@purdue.edu>
2025-11-06 14:50:57 -0500

    Joystick sample

--- src/main.c
+++ src/main.c
@@ -5,22 +5,16 @@
 #include "joystick.h"
 
 int main() {
-    stdio_init_all();      // sets up UART for printf (115200 baud by default)
-    joystick_init();       // sets up ADC on GPIO26/27 and pull-up on GPIO28
+    stdio_init_all();
+    joystick_init();
 
-    while (1) {
-        // read raw X
-        adc_select_input(0); // X on ADC0 / GPIO40
-        uint16_t raw_x = 4095 - adc_read();
+    while (true) {
+        int x = sample_x(); // -1, 0, or 1
+        int y = sample_y(); // -1, 0, or 1
+        bool pressed = joystick_select();
 
-        // read raw Y
-        adc_select_input(1); // Y on ADC1 / GPIO41
-        uint16_t raw_y = adc_read();
+        printf("x=%d y=%d select=%d\r\n", x, y, pressed);
 
-        bool S = joystick_select();
-
-        printf("rawX=%u rawY=%u SEL=%d\r\n", raw_x, raw_y, S);
         sleep_ms(200);
     }
-    
 }
